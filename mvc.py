b5831cb8efe6ca74b625f3cefd163b383c07e54e
ander1119 <b07902060@ntu.edu.tw>
2021-06-27 20:54:35 +0800

    mvc

--- mvc.py
+++ mvc.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np 
+import matplotlib.pyplot as plt
 
 def boundedPoint(selectedPoints):
     selectedPoints = np.array(selectedPoints)
@@ -7,22 +8,27 @@ def boundedPoint(selectedPoints):
     lineLengths = (lineVecs[:, 0]**2 + lineVecs[:, 1]**2) ** (1/2)
     newPoints = []
     for p, v, l in zip(selectedPoints[:-1], lineVecs, lineLengths):
-        tmp = np.array([i for i in range(int(l))] / l) * np.array(v) + np.array(p)
-        print(tmp)
-        newPoints += [i for i in range(int(l))] / l * v + p
-    newPoints += [selectedPoints[-1]]
-    return newPoints 
+        tmp = [list(np.array(i / l * v + p, int)) for i in range(int(l))]
+        # print(tmp)
+        newPoints += tmp
+    newPoints += [list(selectedPoints[-1])]
+    # print(newPoints)
+    return np.array(newPoints) 
 
 def blend(selectedPoints, srcImg, dstImg):
     selectedPoints = boundedPoint(selectedPoints)
-    polygonMask = cv2.fillPoly(np.zeros_like(srcImg), selectedPoints, (255, 255, 255))
-    r, c = np.where(polygonMask == (255, 255, 255))
+    polygonMask = np.zeros_like(srcImg)
+    cv2.fillPoly(polygonMask, [selectedPoints], (255, 255, 255))
+    r, c = np.where(np.all(polygonMask == (255, 255, 255), axis=-1))
 
-    roiMat = polygonMask[(r, c)] # N x 1 matrix
+    roiValueMat = srcImg[(r, c)] 
+    roiPosMat = np.column_stack((r, c)) # N x 1 matrix
     boundMat = np.array(selectedPoints) # M x 1 matrix
-    MVC = np.zeros((len(r), len(selectedPoints)), dtype=float)
 
-    vecMat = roiMat - boundMat[np.newaxis:]
+    vec1 = boundMat[:-1, np.newaxis,:] - roiPosMat[np.newaxis,:,:]
+    vec2 = boundMat[1:, np.newaxis,:] - roiPosMat[np.newaxis,:,:]
+
+    print(vec1.shape)
 
 if __name__ == "__main__":
     selectedPoints = [[187, 738], [91, 533], [90, 224], [143, 104], [317, 4], [521, 93], [592, 303], [553, 544], [407, 771], [271, 770], [187, 738]]
