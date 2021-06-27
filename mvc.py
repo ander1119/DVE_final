4d1765457002c4bc4a58ab0639879199d13f1a3e
ander1119 <b07902060@ntu.edu.tw>
2021-06-27 17:00:51 +0800

    mvc

--- mvc.py
+++ mvc.py
@@ -2,11 +2,14 @@ import cv2
 import numpy as np 
 
 def boundedPoint(selectedPoints):
-    lineVecs = selectedPoints[1:] - selectedPoints[:-2]
+    selectedPoints = np.array(selectedPoints)
+    lineVecs = selectedPoints[1:] - selectedPoints[:-1]
     lineLengths = (lineVecs[:, 0]**2 + lineVecs[:, 1]**2) ** (1/2)
     newPoints = []
-    for p, v, l in zip(selectedPoints[:-2], lineVecs, lineLengths):
-        newPoints += [i in range(int(l))] / l * v + p
+    for p, v, l in zip(selectedPoints[:-1], lineVecs, lineLengths):
+        tmp = np.array([i for i in range(int(l))] / l) * np.array(v) + np.array(p)
+        print(tmp)
+        newPoints += [i for i in range(int(l))] / l * v + p
     newPoints += [selectedPoints[-1]]
     return newPoints 
 
@@ -19,6 +22,13 @@ def blend(selectedPoints, srcImg, dstImg):
     boundMat = np.array(selectedPoints) # M x 1 matrix
     MVC = np.zeros((len(r), len(selectedPoints)), dtype=float)
 
+    vecMat = roiMat - boundMat[np.newaxis:]
+
+if __name__ == "__main__":
+    selectedPoints = [[187, 738], [91, 533], [90, 224], [143, 104], [317, 4], [521, 93], [592, 303], [553, 544], [407, 771], [271, 770], [187, 738]]
+    srcImg = cv2.imread("./image/style1.png")
+    dstImg = cv2.imread("./image/style2.png")
+    blend(selectedPoints, srcImg, dstImg)
     
 
 
